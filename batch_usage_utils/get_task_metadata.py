c0547eb591ffbe91ad633b164fa218f768db9d54
James Chiang <jchiang87@gmail.com>
2025-05-16 07:24:01 -0700

    function to get task names from quantumGraphGeneration.out files; extract butler_metrics if available

--- batch_usage_utils/get_task_metadata.py
+++ batch_usage_utils/get_task_metadata.py
@@ -1,4 +1,5 @@
 import os
+import glob
 from collections import defaultdict
 import json
 import yaml
@@ -10,7 +11,8 @@ from lsst.obs.lsst import LsstCam
 
 
 __all__ = ("extract_metadata", "extract_md_json", "extract_md_yaml",
-           "add_ccd_type", "add_nproc", "add_nproc_avg", "add_node_info")
+           "add_ccd_type", "add_nproc", "add_nproc_avg", "add_node_info",
+           "get_task_names")
 
 
 def extract_metadata(md, data):
@@ -42,12 +44,10 @@ def extract_metadata(md, data):
     # Node info
     if 'nodeName' in info:
         data['node_name'].append(info['nodeName'])
-        data['cpu_count'].append(info['nodeCpuCount'])
-        try:
-            data['cpu_percent'].append(info['endNodeCpuPercent'])
-            data['node_load_avg'].append(info['endNodeLoadAverage'])
-        except KeyError:
-            pass
+    # Butler put/get info
+    if 'butler_metrics' in info:
+        for key in ('time_in_put', 'time_in_get', 'n_get', 'n_put'):
+            data[key].append(info['butler_metrics'][key])
     return data
 
 
@@ -59,7 +59,22 @@ def extract_md_json(json_file, data):
     for key, value in json_obj['metadata']['quantum']['scalars'].items():
         info[key] = value
     for key, value in json_obj['metadata']['quantum']['arrays'].items():
-        info[key] = value[0]
+        try:
+            info[key] = value[0]
+        except IndexError as eobj:
+            pass
+    if 'butler_metrics' not in json_obj['metadata']['quantum']['metadata']:
+        # Do nothing by raising FileNotFoundError to avoid adding dataId info
+        #raise FileNotFoundError("no butler metrics")
+        pass
+    else:
+        info['butler_metrics'] = {}
+        for key, value in json_obj['metadata']['quantum']['metadata']\
+            ['butler_metrics']['scalars'].items():
+            try:
+                info['butler_metrics'][key] = value
+            except IndexError as eobj:
+                pass
     return extract_metadata(md, data)
 
 
@@ -129,3 +144,23 @@ def add_node_info(df0, node_info):
         node_map = dict(zip(node_info['detector'], node_info['worker']))
         df0['worker'] = [node_map[key] for key in df0['detector']]
     return df0
+
+
+def get_task_names(submit_dir_root):
+    log_files = sorted(glob.glob(f"{submit_dir_root}/*/quantumGraph*.out"))
+    tasks = {}
+    for log_file in log_files:
+        with open(log_file) as fobj:
+            lines = fobj.readlines()
+        line_types = [_[:len("Quanta")] for _ in lines]
+        try:
+            index0 = line_types.index("Quanta") + 2
+        except ValueError:
+            continue
+        index1 = line_types[index0:].index("VERBOS") + index0
+        for line in lines[index0:index1]:
+            task = line.strip().split()[-1]
+            count = int(line.strip().split()[0])
+            if task not in tasks:
+                tasks[task] = count
+    return tasks
