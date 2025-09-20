0abfcad3be53d702157aa13c86965fb9a96696c9
James Chiang <jchiang87@gmail.com>
2025-09-19 18:31:24 -0700

    enable job clustering by task

--- batch_usage_utils/computing_cluster.py
+++ batch_usage_utils/computing_cluster.py
@@ -5,6 +5,7 @@ import pickle
 import numpy as np
 import pandas as pd
 from graphlib import TopologicalSorter
+from .job_cluster import JobCluster, make_job_clusters
 
 __all__ = ["ComputingCluster"]
 
@@ -22,8 +23,13 @@ class ComputingCluster:
         self.workflow = None
         self._md = defaultdict(lambda: defaultdict(list))
 
-    def add_job(self, job_name):
-        job = self.workflow.get_job(job_name)
+    def add_jobs(self, job_names):
+        if len(job_names) > 1:
+            job = JobCluster(self.workflow, job_names)
+            job_name = job.id
+        else:
+            job_name = job_names[0]
+            job = self.workflow.get_job(job_name)
         task = job.label
         cpu_time, memory = job.cpu_time, job.memory
         # add random amount of sampling time to the start_time to smooth
@@ -34,12 +40,9 @@ class ComputingCluster:
             return False
         self.running_jobs[job_name] = (requested_cores,
                                        start_time + cpu_time,
-                                       cpu_time)
-        for k, v in job.tags.items():
-            self._md[task][k].append(v)
-        self._md[task]['start_time'].append(start_time)
-        self._md[task]['cpu_time'].append(cpu_time)
-        self._md[task]['memory'].append(memory)
+                                       cpu_time,
+                                       job)
+        job.add_metadata(self._md[task], start_time)
         self.available_cores -= requested_cores
         return True
 
@@ -52,16 +55,16 @@ class ComputingCluster:
     def _compute_available_cores(self):
         occupied_cores = 0
         finished_jobs = []
-        for job, (cores, end_time, cpu_time) in self.running_jobs.items():
+        for job, (cores, end_time, cpu_time, _) in self.running_jobs.items():
             if end_time < self.current_time:
                 finished_jobs.append(job)
             else:
                 occupied_cores += cores
         for job in finished_jobs:
-            cores, _, cpu_time = self.running_jobs[job]
+            cores, _, cpu_time, job_instance = self.running_jobs[job]
             self.total_cpu_time += cores*cpu_time
+            job_instance.notify_ts(self.ts)
             del self.running_jobs[job]
-            self.ts.done(job)
         self.available_cores = self.cores - occupied_cores
 
     @property
@@ -72,10 +75,12 @@ class ComputingCluster:
     def save_md(self, outfile, clobber=False):
         if not clobber and os.path.isfile(outfile):
             raise FileExistsError(f"{outfile} exists already")
+        md = self.md
         with open(outfile, "wb") as fobj:
-            pickle.dump(self.md, fobj)
+            pickle.dump(md, fobj)
 
-    def submit(self, workflow, time_limit=None, outfile=None, shuffle=False):
+    def submit(self, workflow, time_limit=None, outfile=None, shuffle=False,
+               min_cores=5, cluster_defs=None):
         t0 = time.time()
         self.workflow = workflow
         self.ts = TopologicalSorter()
@@ -86,7 +91,7 @@ class ComputingCluster:
         print("TopologicalSorter prep time:", time.time() - t0)
 
         t0 = time.time()
-        job_sequence = list(self.ts.get_ready())
+        job_sequence = make_job_clusters(self.ts.get_ready(), cluster_defs)
         if shuffle:
             np.random.shuffle(job_sequence)
         else:
@@ -94,21 +99,20 @@ class ComputingCluster:
             # that jobs can be popped off the end of the list without
             # invalidating lower-valued indexes.
             job_sequence.reverse()
-        while self.ts.is_active() or self.running_jobs:
-            if self.available_cores > 0 and job_sequence:
-                indexes = list(range(len(job_sequence)))
-                indexes.reverse()  # reverse to enable popping off end of list
-                for i in indexes:
-                    if self.add_job(job_sequence[i]):
-                        job_sequence.pop(i)
-                    if self.available_cores == 0:
-                        break
+        while self.ts.is_active():
+            # Iterate from the end of job_sequence so that successfully
+            # scheduled jobs can be popped off the end of the list.
+            for i in range(len(job_sequence) - 1, -1, -1):
+                if self.add_jobs(job_sequence[i]):
+                    job_sequence.pop(i)
+                if self.available_cores <= min_cores:
+                    break
             if self.current_time % 100 == 0:
                 print(self.current_time, len(job_sequence), flush=True)
             if outfile is not None and self.current_time % 10000 == 0:
                 print("  saving simulation metadata", flush=True)
                 self.save_md(outfile, clobber=True)
-            new_jobs = list(self.ts.get_ready())
+            new_jobs = make_job_clusters(self.ts.get_ready(), cluster_defs)
             if new_jobs:
                 if shuffle:
                     job_sequence.extend(new_jobs)
@@ -116,7 +120,8 @@ class ComputingCluster:
                 else:  # prepend new jobs to job_sequence
                     new_jobs.reverse()
                     job_sequence = new_jobs + job_sequence
-            if time_limit is not None and self.current_time > time_limit:
+            if ((time_limit is not None and self.current_time > time_limit)
+                or not self.running_jobs):
                 break
             self.update_time()
         print("simulated wall time:", self.current_time/3600.)
